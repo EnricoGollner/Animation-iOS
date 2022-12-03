e72bd9eaed3207327234bdcaaf43e67394221892
Enrico Gollner <102383144+EnricoGollner@users.noreply.github.com>
2022-12-02 22:37:29 -0300

    Basic of the technique project

---
+++ Animation/Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Enrico Sousa Gollner on 30/11/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animation/Animation/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  Animation
+//
+//  Created by Enrico Sousa Gollner on 30/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View{
+    @State private var animationAmount = 1.0
+    
+    var body: some View{
+        Button("Tap me"){
+            
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .overlay(
+            Circle()
+                .stroke(.blue)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(.easeOut(duration: 1)
+                    .repeatForever(autoreverses: false),
+                           value: animationAmount)
+        )
+        .onAppear{
+            animationAmount = 2
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
